7ec76117613a7f62e1e4a3c588e0613771553b20
nuccipg72 <nuccipg@gmail.com>
2026-08-16 23:16:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rete prima, cache come rete di scorta: online prendi sempre l'ultima
    versione pubblicata, offline parte comunque l'ultima che hai aperto.
    Il contrario (cache prima) ti bloccherebbe su una versione vecchia. */
-const CACHE = 'campo-vivo-v45';
+const CACHE = 'campo-vivo-v46';
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
